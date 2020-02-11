2ece17714d9f718870a0099a72046400552de01a
unknown <m.khmalech@gmail.com>
2020-02-11 13:41:05 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,55 @@
+// User Module here we stock every 
+// data recorded from user
+type UserEmail = string
+type UserPassword = string
+type UserCode = number
+type UserCreatedAt = Date
+
+// User entry data
+
+//== Profile
+type UserProfileFirstName = string
+type UserProfileLastName = string
+type UserProfileMiddleName =string
+
+//== Account
+type UserAccountTypeCode = number
+type UserAccountTypeName = string
+
+interface UserAccountType {
+    typeCode : UserAccountTypeCode
+    typeName : UserAccountTypeName
+}
+// user session 
+// last connected
+
+/// Profile Parameters
+
+interface ProfileName {
+    first_Name : UserProfileFirstName
+    last_Name : UserProfileLastName
+    middleName? : UserProfileMiddleName
+}
+// Profile 
+// is data information about 
+// user address location male female age
+interface Profile {
+    name : ProfileName
+}
+
+// Account Type :
+// User Create Account type which decide if he 
+// is a doctor patient ....
+
+interface Account {
+    type : UserAccountType
+}
+
+
 interface DecodeToken {
     userId: string,
     email: string,
     iat?: number | string,
     exp?: number | string
 }
+
